0c124d12f14da8cff5e4e7e6de135d75f96b93c2
saldray <othernother@yandex.com>
2025-11-12 13:24:43 +0300

    merak ettigim

---
+++ Alistirma/Geeks_ArrayPrint/Geeks.java
@@ -0,0 +1,13 @@
+public class Geeks {
+    public static void main(String[] args) {
+        //initialize array
+        int[] arr = {40, 55, 63, 17, 22};
+
+        //size of array
+        int uzunluk = arr.length;
+
+        for(int i = 0;i<uzunluk;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
